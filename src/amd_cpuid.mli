788840d4b28a999edf11bfb8236b85da9905ec5c
public-release <as-public-release@janestreet.com>
2025-04-28 19:35:05 +0100

    v0.18~preview.130.26+1192

--- src/amd_cpuid.mli
+++ src/amd_cpuid.mli
@@ -1,15 +1,11 @@
-(**
-   All of the information in this file comes from (currently) page 1779 of the AMD
-   Architecture Programmer's Manual, Volumes 1-5. Specifically, Volume 3: Appendix E
-   detailing the CPUID instruction.
- **)
+(** All of the information in this file comes from (currently) page 1779 of the AMD
+    Architecture Programmer's Manual, Volumes 1-5. Specifically, Volume 3: Appendix E
+    detailing the CPUID instruction. **)
 
 include Cpuid_intf.S
 
-(**
-   On AMD CPUs the leaf retrieved when EAX is set to 0x1 contains processor model
-   information as well as some basic features about the processor.
- **)
+(** On AMD CPUs the leaf retrieved when EAX is set to 0x1 contains processor model
+    information as well as some basic features about the processor. **)
 module Version_and_feature_information : sig
   module Eax : sig
     type t =
@@ -22,11 +18,9 @@ module Version_and_feature_information : sig
     [@@deriving sexp_of]
   end
 
-  (**
-     This is the only field in the CPUID which changes when called multiple times. The
-     [initial_apic_id] field changes based on which core the process is currently running
-     on.
-   **)
+  (** This is the only field in the CPUID which changes when called multiple times. The
+      [initial_apic_id] field changes based on which core the process is currently running
+      on. **)
   module Ebx : sig
     type t =
       { mutable brand_index : int
@@ -101,24 +95,19 @@ module Version_and_feature_information : sig
   end
 end
 
-(**
-   Canonical identifier similar to the one we use on Intel CPUs to index into
-   a list of supported performance counters.
+(** Canonical identifier similar to the one we use on Intel CPUs to index into a list of
+    supported performance counters.
 
-   See [Intel_cpuid.canonical_identifier] for a description of that.
- **)
+    See [Intel_cpuid.canonical_identifier] for a description of that. **)
 val canonical_identifier : t -> string
 
-(**
-   The leaf at 0x7 is special because in addition to putting 0x7 it also lets you
-   put a value into ECX to get a particular feature subleaf. In this case we want
-   ECX=0x0, and any processor which supports EAX=0x7 (which we do check) supports
-   ECX=0x0.
+(** The leaf at 0x7 is special because in addition to putting 0x7 it also lets you put a
+    value into ECX to get a particular feature subleaf. In this case we want ECX=0x0, and
+    any processor which supports EAX=0x7 (which we do check) supports ECX=0x0.
 
-   This provides flags into more modern features on AMD CPUs, including some which
-   most of our machines definitely do not have. If you use any of these features,
-   check to make sure they're supported.
- **)
+    This provides flags into more modern features on AMD CPUs, including some which most
+    of our machines definitely do not have. If you use any of these features, check to
+    make sure they're supported. **)
 module Extended_feature_flags_subleaf_0 : sig
   module Ebx_flags : sig
     include Flags.S
